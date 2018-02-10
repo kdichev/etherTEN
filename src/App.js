2bbf06f5331f6771bfe2e80dfe113e74231c60d6
Konstantin Dichev <konstantin.dichev@gmail.com>
2018-02-10 17:25:27 +0100

    add transactions connected to block by hash (#12)
    * add transactions connected to block by hash
    
    * clean up

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 import { withWeb3 } from './Web3Provider'
 
 class App extends Component {
-
   state = {
-    blocks: []
+    blocks: [],
+    transactions: []
   }
 
   async componentDidMount() {
@@ -16,23 +16,28 @@ class App extends Component {
     this.handleGetLatestBlocks(blockNumber)
   }
 
-  handleGetLatestBlocks = async (blockNumber) => {
+  handleGetLatestBlocks = async blockNumber => {
     const { web3: { eth } } = this.props
 
     for (let i = 0; i < 10; i++) {
-      const { blocks } = this.state
+      const { blocks, transactions } = this.state
       const newBlock = await eth.getBlock(blockNumber - i)
 
       if (newBlock) {
         this.setState({
-          blocks: [...blocks, newBlock]
+          blocks: [...blocks, newBlock],
+          transactions: [
+            ...transactions, {
+              blockHash: newBlock.hash,
+              transactions: newBlock.transactions
+            }
+          ]
         })
       }
     }
   }
 
   render() {
-    console.log("Blocks: ", this.state.blocks)
     return (
       <div className="App">
         <header className="App-header">
