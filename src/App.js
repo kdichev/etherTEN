299d8fa3762e2d37e84c9180f9527795c833d203
Konstantin Dichev <konstantin.dichev@gmail.com>
2018-02-10 16:50:53 +0100

    Add latest ten blocks (#11)
    * connect to current metamask provider
    
    * log latest 10 blocks
    
    * add handleGetLatestBlocks function

--- src/App.js
+++ src/App.js
@@ -4,10 +4,35 @@ import './App.css';
 import { withWeb3 } from './Web3Provider'
 
 class App extends Component {
-  componentDidMount() {
-    console.log(this.props)
+
+  state = {
+    blocks: []
   }
+
+  async componentDidMount() {
+    const { web3: { eth } } = this.props
+
+    const blockNumber = await eth.getBlockNumber()
+    this.handleGetLatestBlocks(blockNumber)
+  }
+
+  handleGetLatestBlocks = async (blockNumber) => {
+    const { web3: { eth } } = this.props
+
+    for (let i = 0; i < 10; i++) {
+      const { blocks } = this.state
+      const newBlock = await eth.getBlock(blockNumber - i)
+
+      if (newBlock) {
+        this.setState({
+          blocks: [...blocks, newBlock]
+        })
+      }
+    }
+  }
+
   render() {
+    console.log("Blocks: ", this.state.blocks)
     return (
       <div className="App">
         <header className="App-header">
