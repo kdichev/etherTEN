62d2663429bfa97467b18a83bbcf736568756c9d
Konstantin Dichev <konstantin.dichev@gmail.com>
2018-02-21 11:23:18 +0100

    remove lints

--- src/components/App/primitives.js
+++ src/components/App/primitives.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import transition from "styled-transition-group";
 
 export const CardsContainer = styled.div`
