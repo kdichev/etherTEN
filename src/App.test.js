9379eef8ca84fda30b8531e8956a0213312a7770
Konstantin Dichev <konstantin.dichev@gmail.com>
2018-02-21 16:05:31 +0100

    more travis setup

--- src/App.test.js
+++ src/App.test.js
@@ -2,20 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
 import Provider from "./components/Web3Provider";
+import { BlockCard } from "./components/BlockCard/BlockCard";
 
-const MockResponse = {
+const mockBlock = {
   difficulty: "2860567509086985",
   hash: "0xf586c298167ca897087694039612ccf18845955f04de6d3b772a4651558e6056",
   miner: "0x52bc44d5378309EE2abF1539BF71dE1b7d7bE3b5",
   number: 5129778,
   timestamp: 1519210152,
-  toggle: false,
   transactions: []
 };
 
 const MockEth = {
   getBlockNumber: () => setTimeout(() => 123, 2000),
-  getBlock: () => setTimeout(() => MockResponse, 2000)
+  getBlock: () => setTimeout(() => mockBlock, 2000)
 };
 
 it("renders App without crashing if Web3 Eth is provided", () => {
@@ -26,6 +26,7 @@ it("renders App without crashing if Web3 Eth is provided", () => {
     </Provider>,
     div
   );
+  expect(div.children).toBeDefined();
   ReactDOM.unmountComponentAtNode(div);
 });
 
@@ -37,5 +38,11 @@ it("renders App without crashing if Web3 Eth is not provided", () => {
     </Provider>,
     div
   );
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders BlockCard with Block data without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<BlockCard {...mockBlock} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
